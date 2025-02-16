233f02a836e16c8c3d9084d6464bd4481effb1a4
Parin <acharyaparin05@gmail.com>
2025-02-17 01:52:45 +0300

    python parser

--- src/context/language/python-parser.ts
+++ src/context/language/python-parser.ts
@@ -1,15 +1,94 @@
-import { AbstractParser, EnclosingContext } from "../../constants";
-export class PythonParser implements AbstractParser {
-  findEnclosingContext(
-    file: string,
-    lineStart: number,
-    lineEnd: number
-  ): EnclosingContext {
-    // TODO: Implement this method for Python
-    return null;
+import { AbstractParser, EnclosingContext, TreeSitterNode } from "../../constants";
+import Parser from "tree-sitter";
+import Python from "tree-sitter-python";
+
+const processNode = (
+  node: Parser.SyntaxNode,
+  lineStart: number,
+  lineEnd: number,
+  largestSize: number,
+  largestEnclosingContext: Parser.SyntaxNode | null
+) => {
+  const { startPosition, endPosition } = node;
+
+  if (startPosition.row + 1 <= lineStart && lineEnd <= endPosition.row + 1) {
+    const size = endPosition.row - startPosition.row;
+    if (size > largestSize) {
+      largestSize = size;
+      largestEnclosingContext = node;
+    }
   }
+  return { largestSize, largestEnclosingContext };
+};
+
+const convertSyntaxNodeToTreeSitterNode = (syntaxNode: Parser.SyntaxNode): TreeSitterNode => {
+  return {
+    type: syntaxNode.type,
+    loc: {
+      start: {
+        line: syntaxNode.startPosition.row + 1,
+        column: syntaxNode.startPosition.column,
+      },
+      end: {
+        line: syntaxNode.endPosition.row + 1,
+        column: syntaxNode.endPosition.column,
+      },
+    },
+  };
+};
+
+export class PythonParser implements AbstractParser {
+  private parser: Parser;
+
+  constructor() {
+    this.parser = new Parser();
+    this.parser.setLanguage(Python);
+  }
+
+  findEnclosingContext(file: string, lineStart: number, lineEnd: number): EnclosingContext {
+    const tree = this.parser.parse(file);
+
+    let largestEnclosingContext: Parser.SyntaxNode | null = null;
+    let largestSize = 0;
+
+    const traverseNodes = (node: Parser.SyntaxNode) => {
+      ({ largestSize, largestEnclosingContext } = processNode(
+        node,
+        lineStart,
+        lineEnd,
+        largestSize,
+        largestEnclosingContext
+      ));
+      for (let i = 0; i < node.childCount; i++) {
+        traverseNodes(node.child(i));
+      }
+    };
+
+    traverseNodes(tree.rootNode);
+
+    return {
+      enclosingContext: largestEnclosingContext
+        ? convertSyntaxNodeToTreeSitterNode(largestEnclosingContext)
+        : null,
+    };
+  }
+
   dryRun(file: string): { valid: boolean; error: string } {
-    // TODO: Implement this method for Python
-    return { valid: false, error: "Not implemented yet" };
+    try {
+      const tree = this.parser.parse(file);
+
+      if (tree.rootNode.hasError) {
+        return {
+          valid: false,
+          error: "Syntax error in Python code",
+        };
+      }
+      return { valid: true, error: "" };
+    } catch (err) {
+      return {
+        valid: false,
+        error: `Error parsing Python code: ${err.message}`,
+      };
+    }
   }
 }
